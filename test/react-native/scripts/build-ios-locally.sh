abcc41c95344c6aa9731be95c1e0f7fc3e9f89be
Alex Moinet <alex@bugsnag.com>
2019-09-12 16:00:28 +0100

    React-native buildkite: Added scripts for building react-native ipa

--- test/react-native/scripts/build-ios-locally.sh
+++ test/react-native/scripts/build-ios-locally.sh
@@ -1,13 +1,6 @@
 #!
-cd test/expo/features/fixtures/test-app && \
-npm i turtle-cli bunyan && \
-perl -0777 -i.original -pe "s/entitlements\\['aps-environment'\\] =[^;]+;//gs" node_modules/turtle-cli/node_modules/xdl/build/detach/IosNSBundle.js && \
-node_modules/.bin/turtle build:ios \
-  -c test/expo/features/fixtures/test-app/app.json \
-  --team-id $APPLE_TEAM_ID \
-  --dist-p12-path $EXPO_P12_PATH \
-  --provisioning-profile-path $EXPO_PROVISIONING_PROFILE_PATH \
-  --release-channel $EXPO_RELEASE_CHANNEL \
-  -o build/output.ipa
+cp -r test/react-native/features/fixtures/app test/react-native/features/fixtures/$REACT_NATIVE_VERSION/app
+cd test/react-native/features/fixtures/$REACT_NATIVE_VERSION
+source build.sh
 
-docker-compose build expo-maze-runner
+cp output/output.ipa ../../../../../build/$REACT_NATIVE_VERSION.ipa
