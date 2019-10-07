d4a24ddfd8b750779588326d1d93d50aa9894393
Alex Moinet <alex@bugsnag.com>
2019-10-07 14:43:21 +0100

    React-native buildkite: Minor test updates

--- test/react-native/features/steps/react-native-steps.rb
+++ test/react-native/features/steps/react-native-steps.rb
@@ -46,12 +46,12 @@ end
 
 Then("the event {string} equals one of:") do |field_path, table|
   actual_value = read_key_path(Server.current_request[:body], "events.0.#{field_path}")
-  valid_values = table.raw
+  valid_values = table.raw.flatten
   assert_true(valid_values.include?(actual_value), "#{field_path} value: #{actual_value} did not match the given list: #{valid_values}")
 end
 
 Then("the payload field {string} equals one of:") do |field_path, table|
   actual_value = read_key_path(Server.current_request[:body], field_path)
-  valid_values = table.raw
+  valid_values = table.raw.flatten
   assert_true(valid_values.include?(actual_value), "#{field_path} value: #{actual_value} did not match the given list: #{valid_values}")
 end
